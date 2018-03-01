edfd2b8af0f360268b0e9e82e2b15882b47d126e
Momanyi <jjmomanyis@gmail.com>
2018-03-01 09:20:30 +0300

    build apk

--- app/src/main/java/asuper/maathis/maathai/URLs.java
+++ app/src/main/java/asuper/maathis/maathai/URLs.java
@@ -7,8 +7,8 @@ package asuper.maathis.maathai;
 public class URLs {
 
     //http://code0.co.ke/dkutrest1/pages/index.php
-    public static String main_url="http://192.168.43.245/dkutrest1/android/";
+    public static String main_url="http://192.168.42.49/dkutrest1/android/";
     //http://localhost/cladi/pages/images/
-    public static String img_url="http://192.168.43.245/dkutrest1/pages/images/";
+    public static String img_url="http://192.168.42.49/dkutrest1/pages/images/";
 }
 
